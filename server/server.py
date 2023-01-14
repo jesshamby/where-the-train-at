7aecffcbdc5dff48f49cfb39741d000ca1977216
Ben <ben@Bens-MacBook-Pro.local>
2023-01-14 17:08:56 +0000

    Added Flask CORS support

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 import methods
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/arrivals", methods=['GET'])
 def arrivals():
