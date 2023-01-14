f0d719c2af0134a6e897de0366591f62c7efe067
Ben <ben@wn2000rptv2.home>
2023-01-14 11:53:08 +0000

    Added requests endpoint to Flask server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask import request
 import methods
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/arrivals", methods=['GET'])
+def arrivals():
+    stop_point_id = request.args.get('stopPointId', '')
+    platform_name = request.args.get('platformName')
+    return methods.get_arrivals(stop_point_id, platform_name)
 
-@app.route("/sayhi")
-def say_hi():
-    return "<p>hi!</p>"
+@app.route("/test", methods=['GET'])
+def test():
+    return request.args.get('testParam', 'DefaultValHere')
