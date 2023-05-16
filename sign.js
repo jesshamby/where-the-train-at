9b000c60f1e7e3c4829f2bc89832c77bb655cbf2
Ben Roberts <broberts98@hotmail.co.uk>
2023-05-16 22:16:30 +0100

    Changed position of sign on screen and animated second stop showing

--- sign.js
+++ sign.js
@@ -1,14 +1,20 @@
-let platform = "Northbound%20-%20Platform%201";
-let stopPointId = "940GZZLUBLR"
+//#region - declair variables and get data immediately
+
+let platform = "Eastbound%20-%20Platform%201";
+let stopPointId = "940GZZLULVT"
 let arrivals = [];
 let dataPosition = 1;
 let time = '';
+let refreshData = false;
+let conveyor = document.getElementById("animate");
+let timeButton = document.querySelector(".row-time");
 
-// Initial requests
 getData();
 cycleData();
 
-// Update time @ 10Hz
+//#endregion
+
+//#region - update time and fetch data
 setInterval(() => {
   let currentdate = new Date(); 
   let newTime = currentdate.toLocaleTimeString("en-GB");
@@ -18,12 +24,18 @@ setInterval(() => {
   }
 }, 100)
 
-// Fetch & cycle bottom row every 10 seconds 
 setInterval(() => {
-  getData();
-  cycleData();
-}, 10000)
+  conveyor.classList.toggle("scroll-in");
+  refreshData = !refreshData;
+  if (refreshData) {
+    getData();
+    cycleData();
+  }
+}, 2500)
 
+//#endregion
+
+//#region - functions
 async function getData() {
   fetch(`http://127.0.0.1:5000/arrivals?stopPointId=${stopPointId}&platformName=${platform}`)
     .then((response) => {
@@ -33,7 +45,6 @@ async function getData() {
       arrivals = data;
       document.querySelector('.destination1').textContent = data[0]["towards"];
       document.querySelector('.arrival1').textContent = data[0]["timeToStation"] + " mins";
-      //cycleData();
     })
 }
 
@@ -46,8 +57,17 @@ function changePosition() {
 }
 
 function cycleData() {
-  document.querySelector('.position2').textContent = (dataPosition + 1);
-  document.querySelector('.destination2').textContent = arrivals[dataPosition]["towards"];
-  document.querySelector('.arrival2').textContent = arrivals[dataPosition]["timeToStation"] + " mins";
-  changePosition();
+  try{
+    document.querySelector('.position2').textContent = (dataPosition + 1);
+    document.querySelector('.destination2').textContent = arrivals[dataPosition]["towards"];
+    document.querySelector('.arrival2').textContent = arrivals[dataPosition]["timeToStation"] + " mins";
+    changePosition();
+  }
+  catch {
+    console.log("Could not get other times.");
+  }
 }
+
+//#endRegion
+
+console.log("hi")
